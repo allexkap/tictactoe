b68c86d9f904acefe85f33636d01a8b19468ecb8
allexkap <aekpstn@gmail.com>
2023-06-07 03:26:38 +0300

    add c gen

--- gen.py
+++ gen.py
@@ -8,8 +8,8 @@ class Field:
     def __getitem__(self, arg):
         return self.state // 3**arg % 3
 
-    def __str__(self):
-        return '[%s]\n' % ']\n['.join(']['.join(self.ch(i, j) for i in range(3)) for j in range(3))
+    def __repr__(self):
+        return '[%s]\\n' % ']\\n['.join(']['.join(self.ch(i, j) for i in range(3)) for j in range(3))
 
     def __and__(self, other):
         return all(not other[i] or self[i] == other[i] for i in range(9))
@@ -27,3 +27,31 @@ def check(field):
         if any(field & Field(state*i) for state in wins):
             return i
     return 0
+
+
+def act(field, deep=0):
+    if deep == 6: return
+    print('printf("\33c%s");' % field)
+    if c := check(field):
+        print('return 0;')
+        return
+    print('scanf("%d", &value);')
+    print('switch (value) {')
+    for i in range(9):
+        if field[i]: continue
+        print(f'case {i+1}:')
+        act(field.add(i, deep%2+1), deep+1)
+        print('break;')
+    print('default:')
+    print('return 1;')
+    print('}')
+
+
+
+import sys
+sys.stdout = open('auto.c', 'w')
+
+field = Field()
+print('#include <stdio.h>\nint main() {\nint value;')
+act(field)
+print('return 2;\n}')
